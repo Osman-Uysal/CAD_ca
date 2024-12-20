30a9482f0dfa34a011a5f735d705d0cac773e64c
Osman Uysal <osmanuysalca@outlook.com>
2024-12-20 18:26:05 +0000

    new offenses

--- carsApp/config/environments/development.rb
+++ carsApp/config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.hosts = ["ec2-3-80-117-14.compute-1.amazonaws.com"]
+  config.hosts = ['ec2-3-80-117-14.compute-1.amazonaws.com']
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
